991c3b5f6d926b562165438a315994a1d951ebda
Santi-Bobadilla <santiagobobadilla@gmail.com>
2022-12-26 04:04:16 -0300

    Frontend 1.0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { HardSoftSkillComponent } from './componentes/hard-soft-skill/hard-soft-skill.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HardSoftSkillComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componentes/hard-soft-skill/hard-soft-skill.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hard-soft-skill',
+  templateUrl: './hard-soft-skill.component.html',
+  styleUrls: ['./hard-soft-skill.component.css']
+})
+export class HardSoftSkillComponent {
+
+}
